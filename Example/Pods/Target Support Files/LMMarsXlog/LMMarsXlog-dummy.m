57dadcd1c61e5d666af92f75895903f0cf50f21a
boluo <xiyouji.xiaoyaoyou.im980@gmail.com>
2021-01-20 11:52:24 +0800

    first commit

---
+++ Example/Pods/Target Support Files/LMMarsXlog/LMMarsXlog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LMMarsXlog : NSObject
+@end
+@implementation PodsDummy_LMMarsXlog
+@end
---
+++ LMMarsXlog/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "feb3ecbd"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2021-01-16 16:57:05"
+#define MARS_TAG ""
+
+#endif
